d9e3c703b1d8b37ebb9e3e146441ad5cc0bd3590
Olasoji Amujo <53713710+Olasojiamujo@users.noreply.github.com>
2024-03-08 21:52:52 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 from urlshort import create_app
+from flask_sqlalchemy import SQLAlchemy
 
 app = create_app()
+
+db = SQLAlchemy(app)
+
+# Initialize the database with your Flask app
+db.init_app(app)
