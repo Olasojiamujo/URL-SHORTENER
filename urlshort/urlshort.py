d6dd1e47d78e47a988ba7cb20bc963c73b441ef7
Olasoji Amujo <53713710+Olasojiamujo@users.noreply.github.com>
2024-03-08 21:50:21 +0000

    Update urlshort.py

--- urlshort/urlshort.py
+++ urlshort/urlshort.py
@@ -11,8 +11,17 @@ app.config["SQLALCHEMY_DATABASE_URI"]=os.environs.get("DATABASE_URL")
 #replaced by Blueprint
 #app = Flask(__name__) #name of the modeule running the Flask
 #app.secret_key = 'h432hi5ohi3h5i5hi3o2hi'
-
 #print(__name__)
+
+class URL(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(80), unique=True, nullable=False)
+    url = db.Column(db.String(255))
+    file_path = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<URL %r>' % self.code
+
 
 #creating a route
 @bp.route('/') #redirect to the base url.
@@ -23,45 +32,39 @@ def home(): #this is a regular python code
 @bp.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
-        urls ={}
+        code = request.form['code']
+        existing_url = URL.query.filter_by(code=code).first()
 
-        if os.path.exists('urls.json'):
-            with open('urls.json') as urls_file:
-                urls = json.load(urls_file)
-
-        if request.form['code'] in urls.keys():
-            flash('That Shortname already been taken. Please use another short name')
+        if existing_url:
+            flash('That Shortname has already been taken. Please use another short name')
             return redirect(url_for('urlshort.home'))
 
-        if 'url' in request.form.keys():
-            urls[request.form['code']] = {'url':request.form['url']}
+        if 'url' in request.form:
+            new_url = URL(code=code, url=request.form['url'])
         else:
             f = request.files['file']
-            full_name = request.form['code'] + secure_filename(f.filename)
+            full_name = code + secure_filename(f.filename)
             f.save('./static/user_files/' + full_name)
-            urls[request.form['code']] = {'file':full_name}
+            new_url = URL(code=code, file_path=full_name)
 
-
-        with open('urls.json', 'w') as url_file:
-            json.dump(urls, url_file)
-            session[request.form['code']] = True
-        return render_template('your_url.html', code=request.form['code'])
-        #return render_template('your_url.html', code=request.args['code']) change to form when working with post request
+        db.session.add(new_url)
+        db.session.commit()
+        session[code] = True
+        return render_template('your_url.html', code=code)
     else:
         return redirect(url_for('urlshort.home'))
 
-
 @bp.route('/<string:code>')
 def redirect_to_url(code):
-    if os.path.exists('urls.json'):
-        with open('urls.json') as urls_file:
-            urls = json.load(urls_file)
-            if code in urls.keys():
-                if 'url' in urls[code].keys():
-                    return redirect(urls[code]['url'])
-                else:
-                    return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
+    url = URL.query.filter_by(code=code).first()
+
+    if url:
+        if url.url:
+            return redirect(url.url)
+        else:
+            return redirect(url_for('static', filename='user_files/' + url.file_path))
     return abort(404)
+
 
 @bp.errorhandler(404)
 def page_not_found(error):
