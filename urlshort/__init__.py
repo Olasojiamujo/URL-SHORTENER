311331db7620a12559160fa5f444f1bb2d251a87
Olasoji Amujo <53713710+Olasojiamujo@users.noreply.github.com>
2024-03-08 22:02:49 +0000

    Update __init__.py

--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__) #name of the modeule running the Flask
-    app.secret_key = 'h432hi5ohi3h5i5hi3o2hi'
 
     db = SQLAlchemy(app)
 
