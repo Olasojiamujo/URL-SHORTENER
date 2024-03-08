5fd6a7b279641db981c62f57a936007dc19de100
Olasoji Amujo <53713710+Olasojiamujo@users.noreply.github.com>
2024-03-08 22:16:23 +0000

    Update __init__.py

--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Create the SQLAlchemy db instance globally
+db = SQLAlchemy()
 
 def create_app(test_config=None):
-    app = Flask(__name__) #name of the modeule running the Flask
+    app = Flask(__name__) # Name of the module running the Flask app
 
-    db = SQLAlchemy(app)
+    # Configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")  # Example for SQLite, adjust for your DB
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional: but useful to suppress a warning
+    #app.secret_key = 'your_secret_key'  # Set a secret key for sessions and cookies
 
     # Initialize the database with your Flask app
     db.init_app(app)
-    
+
+    with app.app_context():
+        db.create_all()  # Create database tables for our data models
+
     from . import urlshort
     app.register_blueprint(urlshort.bp)
 
