823999933547d9376eef9d867090be6089449261
Olasoji Amujo <53713710+Olasojiamujo@users.noreply.github.com>
2024-03-09 14:28:23 +0000

    Update urlshort.py

--- urlshort/urlshort.py
+++ urlshort/urlshort.py
@@ -1,20 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify, Blueprint, send_from_directory
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify, Blueprint, send_from_directory 
 import json
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 from werkzeug.utils import secure_filename
-from .__init__ import db
 
+# Create the SQLAlchemy db instance globally
+db = SQLAlchemy()
 
 bp = Blueprint('urlshort', __name__)
 
 class URL(db.Model):
-    __tablename__ = 'url'
+    __tablename__ = 'urls'  # Changed to 'urls' to avoid potential SQL keyword conflict
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(80), unique=True, nullable=False)
     url = db.Column(db.String(255), nullable=True)
-    file_content = db.Column(db.LargeBinary, nullable=True)  # Store file content
-    filename = db.Column(db.String(255), nullable=True)  # Store original filename
+    file_content = db.Column(db.LargeBinary, nullable=True)
+    filename = db.Column(db.String(255), nullable=True)
 
     def __init__(self, code, url=None, file_content=None, filename=None):
         self.code = code
@@ -22,17 +24,13 @@ class URL(db.Model):
         self.file_content = file_content
         self.filename = filename
 
-#creating a route
-@bp.route('/') #redirect to the base url.
-def home(): #this is a regular python code
+@bp.route('/')
+def home():
     return render_template('home.html', codes=session.keys())
-#    return render_template('home.html', name='Ola')
 
-
-@bp.route('/favicon.ico') 
-def favicon(): 
+@bp.route('/favicon.ico')
+def favicon():
     return send_from_directory(os.path.join(bp.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
 
 @bp.route('/your-url', methods=['GET', 'POST'])
 def your_url():
@@ -48,12 +46,15 @@ def your_url():
             new_url = URL(code=code, url=request.form['url'])
         else:
             file = request.files['file']
-            file_content = file.read()
-            new_url = URL(code=code, file_content=file_content, filename=secure_filename(file.filename))
+            if file:  # Check if a file is actually uploaded
+                file_content = file.read()
+                new_url = URL(code=code, file_content=file_content, filename=secure_filename(file.filename))
+            else:
+                flash('No file uploaded.')
+                return redirect(url_for('urlshort.home'))
 
         db.session.add(new_url)
         db.session.commit()
-        session[code] = True
         return render_template('your_url.html', code=code)
     else:
         return redirect(url_for('urlshort.home'))
@@ -65,10 +66,9 @@ def redirect_to_url(code):
         if url_record.url:
             return redirect(url_record.url)
         else:
-            # Handling file serving is more complex. Consider uploading to a service like S3 and serving from there.
-            abort(404)  # Simplified for this example
-    return abort(404)
-
+            abort(404)
+    else:
+        abort(404)
 
 @bp.errorhandler(404)
 def page_not_found(error):
