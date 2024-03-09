66589189e23744041fb830d2aa35adc26cb61e85
Olasoji Amujo <53713710+Olasojiamujo@users.noreply.github.com>
2024-03-09 22:46:34 +0000

    Update models.py

--- urlshort/models.py
+++ urlshort/models.py
@@ -3,12 +3,12 @@ from .extensions import db
 class SHORTNAME(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(80), unique=True, nullable=False)
-    user_url = db.Column(db.String(255), nullable=True)
+    url = db.Column(db.String(255), nullable=True)
     file_content = db.Column(db.LargeBinary, nullable=True)
     filename = db.Column(db.String(255), nullable=True)
 
-    def __init__(self, code, user_url=None, file_content=None, filename=None):
+    def __init__(self, code, url=None, file_content=None, filename=None):
         self.code = code
-        self.user_url = user_url
+        self.url = url
         self.file_content = file_content
         self.filename = filename
