ee77f92998efb460970f0d7587544bf43ba17aac
Olasoji Amujo <53713710+Olasojiamujo@users.noreply.github.com>
2024-03-09 23:04:36 +0000

    Update __init__.py

--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, render_template
 from .extensions import db
 from .urlshort import bp
@@ -9,10 +8,9 @@ def create_app(test_config=None):
     app = Flask(__name__) # Name of the module running the Flask app
 
     # Configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")  # Example for SQLite, adjust for your DB
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")  # set the DATABASE_URL in your environment
     app.secret_key = 'your_secret_key'  # Set a secret key for sessions and cookies
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
     # Initialize the database with your Flask app
     db.init_app(app)
@@ -20,20 +18,7 @@ def create_app(test_config=None):
     app.register_blueprint(bp)
 
     with app.app_context():
-        db.create_all()
-        db.session.commit()
-
-    # Application-wide error handler
-    @app.errorhandler(404)
-    def page_not_found_appwide(error):
-        return render_template('page_not_found.html'), 404
-
-    @app.errorhandler(Exception)
-    def handle_exception(error):
-
-        # Log the error for debugging
-        app.logger.error(f'Unhandled Exception: {error}')
-
-        return render_template('page_not_found.html'), 500
+        db.create_all() #to create your database
+        db.session.commit() #always remember to commit
 
     return app
